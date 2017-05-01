bd5a57396bb4fdf051cfb347af9c38f495213f70
vicki alden <vic.alden23@gmail.com>
2017-05-01 13:28:55 -0700

    handle server side get request

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ app.use(express.static(__dirname + '/public'));
 app.listen(2023, function() {
   console.log('LISTENING TO PORT NUMBER 2023');
 });
+
+app.get('/list', function(req, res) {
+  List.find({}).exec(function(err, todos) {
+    res.status(200).send(todos);
+  })
+});
